b2456966d75e4f2a816567035a8eb9ec427bed25
Vicente Gutiérrez <vinzegtz@gmail.com>
2020-06-04 17:33:30 +0000

    Add config to weather command

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('What is the weather?');
+const argv = require('./lib/yargs').argv;
+
+const command = argv._[0];
+
+switch (command) {
+    case 'weather':
+        console.log(`City: ${argv.city}`);
+        break;
+    
+    default:
+        console.log('Invalid command');
+        break;
+}
