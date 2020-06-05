5618972588f170fc0c40c26d51c9dde78acbbde1
Vicente Gutiérrez <vinzegtz@gmail.com>
2020-06-05 17:42:09 +0000

    Add OpenCage integration to get coordetanes from a city

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const argv = require('./lib/yargs').argv;
+const openCage = require('./lib/openCage')
+
+const getWeather = async (city) => {
+    const coordenates = await openCage.getCoordenates(city);
+
+    console.log(`City: ${argv.city}`);
+    console.log(`Lat: ${coordenates.lat}, Lon: ${coordenates.lon}`);
+}
 
 const command = argv._[0];
 
 switch (command) {
     case 'weather':
-        console.log(`City: ${argv.city}`);
+        getWeather(argv.city)
         break;
     
     default:
