3a798338ce5ad1b89f6ae96d55a773dfe3eec083
Vicente Gutiérrez <vinzegtz@gmail.com>
2020-06-05 17:59:43 +0000

    Add OpenWeather integration

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const argv = require('./lib/yargs').argv;
-const openCage = require('./lib/openCage')
+const openCage = require('./lib/openCage');
+const openWeather = require('./lib/openWeather');
 
 const getWeather = async (city) => {
     const coordenates = await openCage.getCoordenates(city);
+    const temperature = await openWeather.getCurrentTemperatureByCoord(coordenates.lat, coordenates.lon);
 
     console.log(`City: ${argv.city}`);
     console.log(`Lat: ${coordenates.lat}, Lon: ${coordenates.lon}`);
+    console.log(`Temperature: ${temperature}°`);
 }
 
 const command = argv._[0];
